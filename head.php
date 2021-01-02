6a7b2bf5f385b593f5bbfa785ae1a6a14eb63f59
Rozenn Dahyot <39035808+Roznn@users.noreply.github.com>
2021-01-02 15:06:40 +0000

    update

---
+++ head.php
@@ -0,0 +1,32 @@
+<header id="header">
+	<a href="https://roznn.github.io/" class="logo">roznn.github.io</a>
+	<ul class="icons">
+		<li>
+			<a href="https://twitter.com/RDahyot" class="icon fa-twitter-square" target="_blank"><span class="label">Twitter</span></a>
+		</li>
+		<li>
+			<a href="https://ie.linkedin.com/in/rozenn-dahyot-a0807116" class="icon fa-linkedin-square" target="_blank"><span class="label">LinkedIn</span></a>
+		</li>
+		<li>
+			<a href="http://scholar.google.com/citations?user=nUbM7GYAAAAJ" class="icon fa-google" target="_blank"><span class="label">Google</span></a>
+		</li>
+
+	<li>
+			<a href="http://orcid.org/0000-0003-0983-3052" class="icon fa-link" target="_blank"><span class="label">ORCid</span></a> 
+
+ </li>
+
+<li>
+			<a href="https://github.com/Roznn" class="icon fa-github" target="_blank"><span class="label">Github</span></a>
+		</li>
+<li>
+			<a href="https://www.youtube.com/playlist?list=PLOzfdxcGgy2fKhwTHp7exobdXWfdWTWWx" class="icon fa-youtube-square" target="_blank"><span class="label">Youtube</span></a>
+		</li>
+		
+
+	<!--	<li>
+			<a href="http://tcd.academia.edu/RozennDahyot" class="icon fa-adn" target="_blank"><span class="label">Academia</span></a>
+		</li>
+-->
+	</ul>
+</header>
